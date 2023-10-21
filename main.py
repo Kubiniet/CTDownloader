dc4ed8f8382e4f249772c980ddc0fee064aa6deb
Франциско Давид Гонсалес <fdavid.gonzalez@yandex.ru>
2023-10-22 00:44:21 +0500

    added list option

--- main.py
+++ main.py
@@ -12,32 +12,45 @@ window_width = 1000
 window_height = 800
 
 
-def download(sender, app_data, user_data):
+def download_mp4(sender, app_data, user_data):
     video = YouTube(user_data)
     video = video.streams.get_highest_resolution()
     video.download()
 
 
+def download_mp3(sender, app_data, user_data):
+    video = YouTube(user_data)
+    stream = video.streams.filter(only_audio=True).first()
+    stream.download(filename=f"{video.title}.mp3")
+
+
 def delete_cards():
-    dpg.delete_item("window", children_only=True)
+    dpg.delete_item("cards_container", children_only=True)
 
 
 def show_card_video(url: str, id: int | None):
     yt = YouTube(url)
-    response = requests.get(yt.thumbnail_url)
-    res = yt.streams.get_highest_resolution().resolution
+    try:
+        yt.check_availability()
+        response = requests.get(yt.thumbnail_url)
+        res = yt.streams.get_highest_resolution().resolution
 
-    with open(f"image_{id}.jpg", "wb") as f:
-        f.write(response.content)
-    img = Image.open(f"image_{id}.jpg")
-    img_resized = img.resize((320, 180))
-    img_resized.save(f"image_{id}.jpg")
-    width, height, channels, data = dpg.load_image(f"image_{id}.jpg")
-    if not dpg.get_item_alias(f"image_id_{id}"):
-        with dpg.texture_registry():
-            dpg.add_dynamic_texture(width, height, data, tag=f"image_id_{id}")
+        with open(f"image_{id}.jpg", "wb") as f:
+            f.write(response.content)
+        img = Image.open(f"image_{id}.jpg")
+        img_resized = img.resize((320, 180))
+        img_resized.save(f"image_{id}.jpg")
+        width, height, channels, data = dpg.load_image(f"image_{id}.jpg")
+        if not dpg.get_item_alias(f"image_id_{id}"):
+            with dpg.texture_registry():
+                dpg.add_dynamic_texture(width, height, data, tag=f"image_id_{id}")
+        else:
+            dpg.set_value(f"image_id_{id}", data)
         with dpg.group(
-            horizontal=True, tag=f"card_{id}", parent="main_menu", pos=[250, 250]
+            horizontal=True,
+            tag=f"card_{id}",
+            parent="cards_container",
+            pos=[250, 250],
         ):
             dpg.add_text(
                 f"{yt.title} {yt.length//60}:{yt.length%60}",
@@ -58,46 +71,65 @@ def show_card_video(url: str, id: int | None):
                 pos=[window_width - 50 - width, 250 + 200 * id],
                 parent=f"card_{id}",
             )
-            button_download = dpg.add_button(
-                label=f"Download {res}",
-                callback=download,
-                user_data=url,
+            dpg.add_text(
+                "Download",
+                tag=f"download_label_{id}",
+                parent=f"card_{id}",
                 pos=[50, 380 + 200 * id],
+            )
+            download_video = dpg.add_button(
+                label=f"{res} mp4",
+                callback=download_mp4,
+                user_data=url,
+                pos=[200, 380 + 200 * id],
                 parent=f"card_{id}",
             )
-            dpg.bind_item_font(button_download, default_font)
-            dpg.bind_item_theme(button_download, "__button_theme")
-
-    else:
-        with dpg.texture_registry():
-            dpg.set_value(f"image_id_{id}", data)
-            dpg.set_value(f"title_video_{id}", yt.title)
-
-    dpg.bind_item_font(f"title_video_{id}", default_font)
-    dpg.bind_item_font(f"duration_video_{id}", default_font)
+            download_audio = dpg.add_button(
+                label=f"mp3",
+                callback=download_mp3,
+                user_data=url,
+                pos=[350, 380 + 200 * id],
+                parent=f"card_{id}",
+            )
+            dpg.bind_item_font(download_audio, default_font)
+            dpg.bind_item_theme(download_audio, "__button_theme_blue")
+            dpg.bind_item_font(download_video, default_font)
+            dpg.bind_item_theme(download_video, "__button_theme")
+            dpg.bind_item_font(f"title_video_{id}", default_font)
+            dpg.bind_item_font(f"duration_video_{id}", default_font)
+            dpg.bind_item_font(f"download_label_{id}", default_font)
+    except Exception as e:
+        print(e)
+        with dpg.group(
+            horizontal=True, tag=f"card_{id}", parent="cards_container", pos=[250, 250]
+        ):
+            dpg.add_text(
+                f"{yt.title} {yt.length//60}:{yt.length%60}",
+                tag=f"title_video_{id}",
+                pos=[50, 250 + 200 * id],
+                wrap=580,
+                parent=f"card_{id}",
+            )
+            dpg.add_text(
+                f"Video is unavaliable",
+                tag=f"no_video_{id}",
+                pos=[50, 280 + 200 * id],
+                wrap=580,
+                parent=f"card_{id}",
+            )
+            dpg.bind_item_font(f"title_video_{id}", default_font)
+            dpg.bind_item_font(f"no_video_{id}", default_font)
 
 
 def check_url():
+    delete_cards()
     url = dpg.get_value("input_url")
     if "list" in url:
-        print("list")
         p = Playlist(url)
-        delete_cards()
         for index, url in enumerate(p.video_urls):
-            try:
-                show_card_video(url, index)
-            except VideoUnavailable:
-                print(f"Video {url} is unavaialable, skipping.")
-            else:
-                print(f"Downloading video: {url}")
-
+            show_card_video(url, index)
     elif "https://www.youtube.com/watch?" in url:
-        try:
-            show_card_video(url, id=0)
-        except VideoUnavailable:
-            print(f"Video {url} is unavaialable, skipping.")
-        else:
-            print(f"Downloading video: {url}")
+        show_card_video(url, id=0)
 
 
 dpg.create_viewport(title="Como te descargo")
@@ -111,18 +143,34 @@ with dpg.font_registry():
     custom_font = dpg.add_font(
         file="fonts\mexican-fiesta\mexican fiesta Bold.ttf", size=80
     )
+with dpg.theme(tag="__button_theme_blue"):
+    with dpg.theme_component(dpg.mvButton):
+        dpg.add_theme_color(dpg.mvThemeCol_Button, (78, 174, 215))
+        dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, (51, 51, 55))
+        dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, (37, 37, 38))
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5)
+        dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 5, 3)
 
-# Main Window
+with dpg.theme(tag="__button_theme"):
+    with dpg.theme_component(dpg.mvButton):
+        dpg.add_theme_color(dpg.mvThemeCol_Button, (255, 35, 0, 250))
+        dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, (51, 51, 55))
+        dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, (37, 37, 38))
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5)
+        dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 5, 3)
+
+
 with dpg.window(tag="main_menu") as main_menu_window:
-    # Main label
+    """Main Window"""
     with dpg.group(horizontal=True):
         title = dpg.add_text(
             default_value="COMO TE DESCARGO",
-            color=(255, 33, 0, 250),
             pos=[window_width // 2 - 280, 50],
         )
         dpg.bind_item_font(title, custom_font)
+
     with dpg.group(horizontal=True):
+        """Input of url and button search"""
         input = dpg.add_input_text(
             default_value="url", tag="input_url", height=50, width=800, pos=[50, 200]
         )
@@ -130,15 +178,8 @@ with dpg.window(tag="main_menu") as main_menu_window:
         button_search = dpg.add_button(label="Search", callback=check_url)
         dpg.bind_item_font(button_search, default_font)
 
-        with dpg.theme(tag="__button_theme"):
-            with dpg.theme_component(dpg.mvButton):
-                dpg.add_theme_color(dpg.mvThemeCol_Button, (255, 35, 0, 250))
-                dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, (51, 51, 55))
-                dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, (37, 37, 38))
-                dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5)
-                dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 5, 3)
-
         dpg.bind_item_theme(button_search, "__button_theme")
+    dpg.add_group(tag="cards_container", parent="main_menu")
 
 
 dpg.set_primary_window(window=main_menu_window, value=True)
